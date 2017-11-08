1c8b50b37a7dbaf34bcfeb2326eecbbe31958fb9
Kate Eldridge <kathryneeldridge@gmail.com>
2017-11-07 21:51:19 -0500

    Set up router and add components
    - add components
    - add filter functionality
    - setup react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ let projectData = [
+        {
+          "id": 1,
+          "title": "Mackenzie",
+          "description": "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
+          "img": "https://bb2.mysd.us/media/gene-cms/b/b/bb2-hp-monday_mono.jpg",
+          "tags": ["react", "javascript", "sass"],
+          "url": "https://www.mackenzie.com",
+        },
+        {
+          "id": 2,
+          "title": "Maddie Style",
+          "description": "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
+          "img": "https://bb2.mysd.us/media/gene-cms/b/b/bb2-hp-monday_mono.jpg",
+          "tags": ["react", "javascript", "css"],
+          "url": "https://www.mackenzie.com",
+        },
+        {
+          "id": 3,
+          "title": "Robert Allen Design",
+          "description": "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
+          "img": "https://bb2.mysd.us/media/gene-cms/b/b/bb2-hp-monday_mono.jpg",
+          "tags": ["react", "javascript", "magento"],
+          "url": "https://www.mackenzie.com",
+        },
+        {
+          "id": 4,
+          "title": "Andersen Tax",
+          "description": "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
+          "img": "https://bb2.mysd.us/media/gene-cms/b/b/bb2-hp-monday_mono.jpg",
+          "tags": ["react", "javascript", "test2"],
+          "url": "https://www.mackenzie.com",
+        },
+        {
+          "id": 5,
+          "title": "Vanguard Promotions",
+          "description": "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
+          "img": "https://bb2.mysd.us/media/gene-cms/b/b/bb2-hp-monday_mono.jpg",
+          "tags": ["test", "javascript", "css"],
+          "url": "https://www.mackenzie.com",
+        }];
+
+ReactDOM.render((
+	<BrowserRouter>
+		<App projects={projectData}/>
+	</BrowserRouter>
+), document.getElementById('root'))

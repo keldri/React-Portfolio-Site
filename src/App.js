b61d89cb6e2fd288a2da337dbb3e7a181adfc945
Kate Eldridge <kathryneeldridge@gmail.com>
2017-12-10 16:06:07 -0500

    Add menu button and logo placeholder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import ProjectDashboard from './Components/ProjectDashboard/ProjectDashboard';
 import Portfolio from './Components/Pages/Portfolio';
+import SingleProjectPage from './Components/Pages/SingleProjectPage';
 import Home from './Components/Pages/Home';
 import Header from './Components/Shared/Header';
 import './styles/styles.css';
@@ -32,7 +33,12 @@ class Main extends Component {
       <main>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path="/" component={(props) => <Portfolio
+          <Route exact path='/projects/:title' component={(props, path) => <SingleProjectPage
+            projects={this.props.projects}
+            value={this.props.value}
+            title={props.match.params.title}
+          >{props.children}</SingleProjectPage>}/>
+          <Route path='/projects' component={(props) => <Portfolio
             projects={this.props.projects}
             value={this.props.value}
           >{props.children}</Portfolio>}/>
